4ff90ee9580aa003cf8a255663f3f97d451d1fda
yfarmland <yfarmland@yfarmland.finance>
2020-09-08 15:33:36 +0200

    test

---
+++ build/precache-manifest.a3d305175964f83f79b39f1c8e341d3e.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126ecd2cc053762a14acc99f7c38105e",
+    "url": "/yfarmland/index.html"
+  },
+  {
+    "revision": "6e0d84ae26b6bcea0aea",
+    "url": "/yfarmland/static/css/main.6c3917f8.chunk.css"
+  },
+  {
+    "revision": "b0690d85927907d0fd9b",
+    "url": "/yfarmland/static/js/0.76a23cdc.chunk.js"
+  },
+  {
+    "revision": "b6acc6e4d9b91c9c7bb9",
+    "url": "/yfarmland/static/js/10.bd635939.chunk.js"
+  },
+  {
+    "revision": "c178649ae0de162657ef6caa733cc024",
+    "url": "/yfarmland/static/js/10.bd635939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c25bee332024a3589c1",
+    "url": "/yfarmland/static/js/11.97eaf091.chunk.js"
+  },
+  {
+    "revision": "ebbe2f391005716b3e43",
+    "url": "/yfarmland/static/js/12.4ff85b07.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/yfarmland/static/js/12.4ff85b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01df7bfe6100c308d1c1",
+    "url": "/yfarmland/static/js/13.14f4ae84.chunk.js"
+  },
+  {
+    "revision": "608dde537c6a0c907c10",
+    "url": "/yfarmland/static/js/14.e2562f1b.chunk.js"
+  },
+  {
+    "revision": "1618282e1d3fff045ab2",
+    "url": "/yfarmland/static/js/3.2eae24f0.chunk.js"
+  },
+  {
+    "revision": "911e97d0d624ae22a5898fe0deef95a3",
+    "url": "/yfarmland/static/js/3.2eae24f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df2b525e87a7c5a1f28",
+    "url": "/yfarmland/static/js/4.563583b7.chunk.js"
+  },
+  {
+    "revision": "655d3b6d19a6f311ebf8edac1ed145e6",
+    "url": "/yfarmland/static/js/4.563583b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4376d8d2e56d120d16",
+    "url": "/yfarmland/static/js/5.a12000bc.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/yfarmland/static/js/5.a12000bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508dc6059f9832234e87",
+    "url": "/yfarmland/static/js/6.b71aa843.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "/yfarmland/static/js/6.b71aa843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92bf86e00cd21704fa7",
+    "url": "/yfarmland/static/js/7.1a76c6a8.chunk.js"
+  },
+  {
+    "revision": "0dabf9569f2eb5c7b3cf",
+    "url": "/yfarmland/static/js/8.244212e4.chunk.js"
+  },
+  {
+    "revision": "932213ff9350889c4ac441758e5e82b6",
+    "url": "/yfarmland/static/js/8.244212e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03692855e6d060978ecb",
+    "url": "/yfarmland/static/js/9.1c8fbb71.chunk.js"
+  },
+  {
+    "revision": "6e0d84ae26b6bcea0aea",
+    "url": "/yfarmland/static/js/main.15ba63b8.chunk.js"
+  },
+  {
+    "revision": "64692798035e9a58defb",
+    "url": "/yfarmland/static/js/runtime-main.6df356f9.js"
+  },
+  {
+    "revision": "eba3b1d0dbd43b9c0e305869bf21c1f7",
+    "url": "/yfarmland/static/media/BAT-logo.eba3b1d0.png"
+  },
+  {
+    "revision": "b873063a7edf5b6695d71e992702e3ae",
+    "url": "/yfarmland/static/media/BPT-logo.b873063a.png"
+  },
+  {
+    "revision": "c2374261b533a7919dffbf757842308e",
+    "url": "/yfarmland/static/media/BUSD-logo.c2374261.png"
+  },
+  {
+    "revision": "93c4bb1a91bc5d37be76559f4bd55e69",
+    "url": "/yfarmland/static/media/COMP-logo.93c4bb1a.png"
+  },
+  {
+    "revision": "64055e3f6cce812a48266ab8b3580c8e",
+    "url": "/yfarmland/static/media/CRV-logo.64055e3f.png"
+  },
+  {
+    "revision": "662e0cc6c6369dea0ec6fd1d3bbbb682",
+    "url": "/yfarmland/static/media/DAI-logo.662e0cc6.png"
+  },
+  {
+    "revision": "c628c96fcbb2b678937e4ff2e68769c7",
+    "url": "/yfarmland/static/media/ENJ-logo.c628c96f.png"
+  },
+  {
+    "revision": "37e4f26b52b7f42f079da7cd30eeaf96",
+    "url": "/yfarmland/static/media/ETH-logo.37e4f26b.png"
+  },
+  {
+    "revision": "8f14871ed722cd4d037c4bae7c443ad9",
+    "url": "/yfarmland/static/media/FTM-logo.8f14871e.png"
+  },
+  {
+    "revision": "3eeb6809e57b52b0f0141a8b22a0ec84",
+    "url": "/yfarmland/static/media/KNC-logo.3eeb6809.png"
+  },
+  {
+    "revision": "4a81c5896c0d611c25e7fb060942f794",
+    "url": "/yfarmland/static/media/LEND-logo.4a81c589.png"
+  },
+  {
+    "revision": "f66b0cf3ab573715ce7fadb693f9702b",
+    "url": "/yfarmland/static/media/LINK-logo.f66b0cf3.png"
+  },
+  {
+    "revision": "48316d69d18e4e1981ea708d4f902156",
+    "url": "/yfarmland/static/media/MANA-logo.48316d69.png"
+  },
+  {
+    "revision": "97560b1b695ed659f4bacf27781c3e84",
+    "url": "/yfarmland/static/media/REN-logo.97560b1b.png"
+  },
+  {
+    "revision": "3efc2ab65d4e281b44c3e5d80c60d733",
+    "url": "/yfarmland/static/media/REP-logo.3efc2ab6.png"
+  },
+  {
+    "revision": "aad58391edb93fb7b2cac3e657a6aa96",
+    "url": "/yfarmland/static/media/SAI-logo.aad58391.png"
+  },
+  {
+    "revision": "e549a565ba8927685127e0821cabf172",
+    "url": "/yfarmland/static/media/SNX-logo.e549a565.png"
+  },
+  {
+    "revision": "4302550f3ee705d689b264dd1dde56d2",
+    "url": "/yfarmland/static/media/SUSD-logo.4302550f.png"
+  },
+  {
+    "revision": "9fc5d5dc2ae05826de68f61a956c487c",
+    "url": "/yfarmland/static/media/TUSD-logo.9fc5d5dc.png"
+  },
+  {
+    "revision": "17fc6bec2658eeaedc1adcca3a41a6ac",
+    "url": "/yfarmland/static/media/UNI-logo.17fc6bec.png"
+  },
+  {
+    "revision": "381a07e5339e43fab10a4b918b2af5ac",
+    "url": "/yfarmland/static/media/USDC-logo.381a07e5.png"
+  },
+  {
+    "revision": "4d06ed03ee9590d36fd7f81e10d97093",
+    "url": "/yfarmland/static/media/USDT-logo.4d06ed03.png"
+  },
+  {
+    "revision": "b24396917a138794160b24ff04a71d80",
+    "url": "/yfarmland/static/media/WorkSans-VariableFont_wght.b2439691.ttf"
+  },
+  {
+    "revision": "c69913c1eea546e75b985783bd6ad8ae",
+    "url": "/yfarmland/static/media/YFI-logo.c69913c1.png"
+  },
+  {
+    "revision": "8cf0b2ad11dbe8685815be8543f4a8ca",
+    "url": "/yfarmland/static/media/ZRX-logo.8cf0b2ad.png"
+  },
+  {
+    "revision": "e881b3eccc8add2401ff9f847ec9eb47",
+    "url": "/yfarmland/static/media/aBAT-logo.e881b3ec.png"
+  },
+  {
+    "revision": "712e0d7f11f5622ccbabe336c7fbce7d",
+    "url": "/yfarmland/static/media/aBUSD-logo.712e0d7f.png"
+  },
+  {
+    "revision": "7d2fd5333d6cc4013a3bb06ec7704789",
+    "url": "/yfarmland/static/media/aDAI-logo.7d2fd533.png"
+  },
+  {
+    "revision": "c24821a43ea5dea7399a75785d4364c5",
+    "url": "/yfarmland/static/media/aETH-logo.c24821a4.png"
+  },
+  {
+    "revision": "6938a4300bcda73ed135133224de258c",
+    "url": "/yfarmland/static/media/aKNC-logo.6938a430.png"
+  },
+  {
+    "revision": "1cc319ec2349b478c0405f863ebc4ebe",
+    "url": "/yfarmland/static/media/aLEND-logo.1cc319ec.png"
+  },
+  {
+    "revision": "1c00e7c46413fe8b24f9966a645167dc",
+    "url": "/yfarmland/static/media/aLINK-logo.1c00e7c4.png"
+  },
+  {
+    "revision": "b091957fa78e53f98fdc0b1971c8e780",
+    "url": "/yfarmland/static/media/aMANA-logo.b091957f.png"
+  },
+  {
+    "revision": "374fe2394bf6ecec4c570be6ede2f689",
+    "url": "/yfarmland/static/media/aMKR-logo.374fe239.png"
+  },
+  {
+    "revision": "7ba733790f20df8a57c4f8be663b956e",
+    "url": "/yfarmland/static/media/aREP-logo.7ba73379.png"
+  },
+  {
+    "revision": "c9de216bc91456f619c61806b5465d46",
+    "url": "/yfarmland/static/media/aSNX-logo.c9de216b.png"
+  },
+  {
+    "revision": "9c874e737af30c3564d69a900cad68c8",
+    "url": "/yfarmland/static/media/aSUSD-logo.9c874e73.png"
+  },
+  {
+    "revision": "803236550813c5c5e72f17ac0530cd91",
+    "url": "/yfarmland/static/media/aTUSD-logo.80323655.png"
+  },
+  {
+    "revision": "99d3189e3b4f318d83c5582436da6c21",
+    "url": "/yfarmland/static/media/aUSDC-logo.99d3189e.png"
+  },
+  {
+    "revision": "a251fdec4a2035ad7cafe19b412345e8",
+    "url": "/yfarmland/static/media/aUSDT-logo.a251fdec.png"
+  },
+  {
+    "revision": "72d1e651b5356ff7eae680c5e3d161e0",
+    "url": "/yfarmland/static/media/aWBTC-logo.72d1e651.png"
+  },
+  {
+    "revision": "ecb77c8725752bc5417f919c4ae5b966",
+    "url": "/yfarmland/static/media/aZRX-logo.ecb77c87.png"
+  },
+  {
+    "revision": "237021c9d3d3a370ceed3a3a23065359",
+    "url": "/yfarmland/static/media/cUSDC-logo.237021c9.png"
+  },
+  {
+    "revision": "dcd437f36621d7168fd04bd635a84e47",
+    "url": "/yfarmland/static/media/cUSDT-logo.dcd437f3.png"
+  },
+  {
+    "revision": "62578f5994645a1825d4829e2c4394b0",
+    "url": "/yfarmland/static/media/coinbaseWalletIcon.62578f59.svg"
+  },
+  {
+    "revision": "64055e3f6cce812a48266ab8b3580c8e",
+    "url": "/yfarmland/static/media/curve.fi-logo.64055e3f.png"
+  },
+  {
+    "revision": "8f14871ed722cd4d037c4bae7c443ad9",
+    "url": "/yfarmland/static/media/fUSD-logo.8f14871e.png"
+  },
+  {
+    "revision": "754cd95ef01395f65bc9259278c2f11a",
+    "url": "/yfarmland/static/media/iDAI-logo.754cd95e.png"
+  },
+  {
+    "revision": "54d70e4e51ca31f8b8d1eb162851dd5f",
+    "url": "/yfarmland/static/media/icn-aethereum.54d70e4e.svg"
+  },
+  {
+    "revision": "62825e60a8dc908aef6082461f302133",
+    "url": "/yfarmland/static/media/icn-ledger.62825e60.svg"
+  },
+  {
+    "revision": "e8109252c38f5f7874506c6731c52292",
+    "url": "/yfarmland/static/media/icn-metamask.e8109252.svg"
+  },
+  {
+    "revision": "b234b2bfa0417c7e8711c3a8d17afeec",
+    "url": "/yfarmland/static/media/portisIcon.b234b2bf.png"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "/yfarmland/static/media/trustWallet.edcc1ab5.png"
+  },
+  {
+    "revision": "acea503241f6c55548e458e71dbab192",
+    "url": "/yfarmland/static/media/wBTC-logo.acea5032.png"
+  },
+  {
+    "revision": "8215855c185176eb79446ce8cc1f3998",
+    "url": "/yfarmland/static/media/walletConnectIcon.8215855c.svg"
+  },
+  {
+    "revision": "237021c9d3d3a370ceed3a3a23065359",
+    "url": "/yfarmland/static/media/ycUSDC-logo.237021c9.png"
+  },
+  {
+    "revision": "dcd437f36621d7168fd04bd635a84e47",
+    "url": "/yfarmland/static/media/ycUSDT-logo.dcd437f3.png"
+  }
+]);
---
+++ build/static/js/14.e2562f1b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpyfarmer=this.webpackJsonpyfarmer||[]).push([[14],{1672:function(p,s){}}]);
+//# sourceMappingURL=14.e2562f1b.chunk.js.map
